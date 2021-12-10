bd7b43a44679eaf3a550017763309c84509500dd
fire9 <fire9@me.com>
2021-12-10 14:43:48 +0800

    import package

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fire9/hello-world-go/doctor"
+)
 
 func main() {
 	// var whatToSay string
-	// whatToSay = "Hellow World again!"
-	whatToSay := "Hellow World again!"
-	fmt.Println("Hello, World!")
-	sayHelloWorld(whatToSay)
-}
-
-func sayHelloWorld(whatToSay string) {
+	// whatToSay = doctor.Intro()
+	whatToSay := doctor.Intro()
 	fmt.Println(whatToSay)
 }
